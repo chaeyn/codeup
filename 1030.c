d9298706e20fe36e10adb22f29fb990e735daafe
채근영 <geunyoung9514@naver.com>
2024-11-20 21:48:03 +0900

    1120

--- 1030.c
+++ 1030.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-    long int a, b;
-    scanf("%ld %ld", &a, &b);
-    printf("%ld", a+b);
+    long long int n;
+    scanf("%lld", &n);
+    printf("%lld", n);
     return 0;
 }
