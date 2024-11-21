71541d6ab0dc442d138c59029bfe151f313f3871
채근영 <geunyoung9514@naver.com>
2024-11-21 15:40:59 +0900

    1121

---
+++ 1054.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int x, y = 0;
+    scanf("%d %d", &x, &y);
+    if (x==1&&y==1)
+    {
+        printf("1");
+    }
+    else{
+        printf("0");
+    }
+    return 0;
+}
