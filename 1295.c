6dd701f9960af90915aa1547abfda9eaf0e2cbf2
chaeyn <chaeyn514@gmail.com>
2024-12-03 21:51:13 +0900

    1203

--- 1295.c
+++ 1295.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 int main(){
-    char str[1001];
-    fgets(str, 1001, stdin);
+    char str[1000] = "";
+    char str2[1000] = "";
+    scanf("%s", str);
     for (int i = 0; str[i] != '\0'; i++)
     {
         if (str[i] >= 65 && str[i] <= 90)
         {
-            printf("%c", str[i]-32);
+            str2[i] = str[i] + 32;
         }
         else if (str[i] >= 97 && str[i] <= 122)
         {
-            printf("%c", str[i]+32);
+            str2[i] = str[i] - 32;
         }
         else
         {
-            printf("%c", str[i]);
+            str2[i] = str[i];
         }
     }
+    printf("%s", str2);
     return 0;
 }
---
+++ 1406.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char w[15];
+    scanf("%s", w);
+    if (strcmp(w, "love") == 0)
+    {
+        printf("I love you.");
+    }
+    return 0;
+}
