15bdfee0fbc3336102fc9f30c54681319c8e13c0
chaeyn <chaeyn514@gmail.com>
2024-12-04 23:04:29 +0900

    1204

--- 1407.c
+++ 1407.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-    char str[100] = "";
-    fgets(str, sizeof(str), stdin);
-    for (int i = 0; i < 100; i++)
-    {
-        if [strcmp(str[i], 32)] == 1)
-        {
-            printf("%c", str[i]);
-        }
-        
-    }
+    char str[100] = ""; // 글자수 최대 100 str정의
+    gets(str); // 입력 받은 문자열을 str에 저장
+    for (int i = 0; i < strlen(str); i++) // strlen은 str 문자열의 크기를 반환
+        if (str[i] == ' ') continue; // 만약 str의 i번재가 공백이면 무시하고 진행
+        else printf("%c", str[i]); // 아니라면 문자 하나 출력
     return 0;
 }
