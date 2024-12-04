2464c44a8d07145a0571d6e6c450547feb2dbcb2
chaeyn <chaeyn514@gmail.com>
2024-12-04 23:09:37 +0900

    1204

--- 1408.c
+++ 1408.c
@@ -1,14 +1,18 @@
 //- 입력받은 문자의 ASCII 코드값 + 2
 //- (입력받은 문자의 ASCII 코드값 * 7) % 80 + 48
 #include <stdio.h>
+#include <string.h>
 int main(){
-    char pw[20];
-    int i = 0;
-    scanf("%s", pw);
-    while (i<=20)
+    char str[20] = "";
+    gets(str);
+    for(int i = 0; i < strlen(str); i++)
     {
-        ("%s", pw[i]+2);
-        i++;
+        printf("%c", str[i]+2);
+    }
+    printf("\n");
+    for (int i = 0; i < strlen(str); i++)
+    {
+        printf("%c", str[i]*7%80+48);
     }
     return 0;
 }
