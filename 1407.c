f5e67892bc479a1c8c53e718a8f302610f694653
chaeyn <chaeyn514@gmail.com>
2024-12-04 12:10:05 +0900

    1204

---
+++ 1407.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[100] = "";
+    fgets(str, sizeof(str), stdin);
+    for (int i = 0; i < 100; i++)
+    {
+        if [strcmp(str[i], 32)] == 1)
+        {
+            printf("%c", str[i]);
+        }
+        
+    }
+    return 0;
+}
---
+++ 1408.c
@@ -0,0 +1,14 @@
+//- 입력받은 문자의 ASCII 코드값 + 2
+//- (입력받은 문자의 ASCII 코드값 * 7) % 80 + 48
+#include <stdio.h>
+int main(){
+    char pw[20];
+    int i = 0;
+    scanf("%s", pw);
+    while (i<=20)
+    {
+        ("%s", pw[i]+2);
+        i++;
+    }
+    return 0;
+}
