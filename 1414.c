f20991bf2d4fcfea8c3ef3c54488ef51ea7fcce4
chaeyn <chaeyn514@gmail.com>
2024-12-05 13:05:31 +0900

    1204

---
+++ 1414.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[101] = "";
+    int r1 = 0;
+    int r2 = 0;
+    scanf("%s", str);   
+    for (int i = 0; i != '0'; i++){
+        if ((str[i] == 'c') || (str[i] == 'C')){
+        r1++;
+        }
+        if (((str[i] == 'C') || (str[i] == 'c')) && ((str[i+1] == 'C') || (str[i+1] == 'c'))){
+        r2++;
+        }
+    }
+    printf("%d", r1);
+    printf("\n");
+    printf("%d", r2);    
+    return 0;
+}
