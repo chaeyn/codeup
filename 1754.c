311ceeb9a1be893dcbc6d9d604cc2f3acd0a8141
chaeyn <chaeyn514@gmail.com>
2024-12-06 15:29:34 +0900

    1206

--- 1754.c
+++ 1754.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 int main(){
-    char a[100], b[100] = "";
+    char a[101], b[101] = "";
     scanf("%s %s", a, b);
-    if (strcmp(a,b)>0)
+    if (strlen(a)>strlen(b))
+        printf("%s %s", b, a);
+    else if (strlen(a)<strlen(b))
+        printf("%s %s", a, b);
+    else{
+    int result = (strcmp(a, b));
+    if (result < 0)
     {
-        printf("%s %s", b,a);
+        printf("%s %s", a, b);
     }
-    else
+    else if (result > 0)
     {
-        printf("%s %s", a,b);
-    }    
+        printf ("%s %s", b, a);
+    }
+    }
     return 0;
 }
 
