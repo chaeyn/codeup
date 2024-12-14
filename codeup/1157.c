02f3f3914e14c88607bab84fc0ab645356172272
채근영 <chaeyn514@gmail.com>
2024-12-14 22:47:28 +0900

    1214

---
+++ codeup/1157.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    float n;
+    scanf("%f", &n);
+    if (50<=n && n<=60)
+    {
+        printf("win");
+    }
+    else
+    {
+        printf("lose");    
+    }
+    return 0;
+}
